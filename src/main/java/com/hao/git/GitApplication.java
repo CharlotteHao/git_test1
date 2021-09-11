59ee0b03708db7f38ad835d1798ebda8977ba9b3
shahao <1771478728@qq.com>
2021-09-11 16:56:41 +0800

    second commit

--- src/main/java/com/hao/git/GitApplication.java
+++ src/main/java/com/hao/git/GitApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitApplication {
     public static void main(String[] args) {
+        System.out.println("git2");
         SpringApplication.run(GitApplication.class,args);
     }
 }
